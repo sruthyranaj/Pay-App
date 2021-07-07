48d64cd7fbfe5a5675925f88f59a380daa574393
sruthy <sruthy@techversantinfo.com>
2021-07-07 05:55:21 +0530

    Added testcase for checking owner invoices

--- owner/tests.py
+++ owner/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+
+client = APIClient()
 
 # Create your tests here.
+class YourTestClass(TestCase):
+    def setUp(self):
+        # Setup run before every test method.
+        pass
+
+    def tearDown(self):
+        # Clean up run after every test method.
+        pass
+
+    def test_get_invoice(self):
+        response = client.get('/invoices/')
+        print(response)
